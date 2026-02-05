51b240571f330551a054dfde9df40baaba2131fb
HydroHackerGammer <alexander_meyer_email@web.de>
2026-02-05 16:13:36 +0000

    added shop and responsive design

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let cheese = 0;
+let cheese = new Number(localStorage.getItem("save"))||0;
 const UserinterfaceCounter = document.getElementById("counter");
+const UserinterfaceShop = document.getElementById("shop")
 
 function updateUserinterface() {
     UserinterfaceCounter.textContent = cheese;
@@ -8,4 +9,15 @@ function updateUserinterface() {
 function cheeseClick() {
     cheese += 1;
     updateUserinterface();
+    localStorage.setItem("save", cheese)
+}
+
+updateUserinterface()
+
+function openShop() {
+    UserinterfaceShop.classList.add("open")
+}
+
+function closeShop() {
+    UserinterfaceShop.classList.remove("open")
 }
